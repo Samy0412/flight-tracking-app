13cdbbcbea2f60ffa03fc68f8a853769fa985bd1
SAMANTHA GADET <samantha@merinio.com>
2023-03-20 19:47:30 -0400

    Change Channel name

--- hooks/ably.hooks.js
+++ hooks/ably.hooks.js
@@ -1,19 +1,19 @@
-import { useState, useEffect } from 'react'
-import { ably } from '../env'
+import { useState, useEffect } from "react";
+import { ably } from "../env";
 
 //set CHANNEL_SCOPE  channel
-const CHANNEL_NAME = '<CHANNEL_NAME>'
+const CHANNEL_NAME =
+  "[product:ably-flightradar24/heathrow-flights]flights:airport:LHR";
 export const useAblyChannel = (channel, dependencies) => {
+  const [onMessage, setOnMessage] = useState("Please wait..");
 
-  const [onMessage, setOnMessage] = useState('Please wait..')
-
-  const [isLoading, setLoading] = useState(true)
+  const [isLoading, setLoading] = useState(true);
   //fetch channel data
-  const [channelData, setChannelData] = useState(null)
+  const [channelData, setChannelData] = useState(null);
 
   useEffect(() => {
-   //add ably connection here 
-  }, dependencies)
+    //add ably connection here
+  }, dependencies);
 
-  return [isLoading, onMessage, channelData]
-}
+  return [isLoading, onMessage, channelData];
+};
